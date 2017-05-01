26264adcae8d06ae770033341b89cd8ddf691cb8
tadkim <icanhear@dgu.edu>
2017-05-02 06:34:15 +0900

    (add): refine felxbox code.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -11,14 +11,17 @@ import { CompleterService, CompleterData } from 'ng2-completer';
 @Component({
     selector: 'search-box',
     template: `
-            <img src="../../assets/images/icons/search.png"/>
-            <ng2-completer [(ngModel)]="searchStr" [datasource]="dataService" [minSearchLength]="0"></ng2-completer>
+            <img (click)="onSearch()" src="../../assets/images/icons/search.png"/>
+            <ng2-completer
+                *ngIf="isSearch"
+                [(ngModel)]="searchStr" [datasource]="dataService" [minSearchLength]="0"></ng2-completer>
     `,
     styleUrls: ['./search.component.css']
 })
     
 export class SearchComponent {
-
+    isSearch: boolean = false;
+    
   public searchStr: string;
   public dataService: CompleterData;
   public searchData;
@@ -31,5 +34,10 @@ export class SearchComponent {
       this.searchData = this.appService.getCups();
       this.dataService = completerService.local(this.searchData, 'name', 'name'); // data, searchTerm, viewTerm.
   }
+
+  onSearch() {
+      this.isSearch = !this.isSearch;
+
+  }  
 
 }
